3290fbb95392119e1f13e15cd32fd10b278e1e0d
Sunil Ganesh Kurra <2000137243@hexaware.com>
2025-03-04 19:25:29 +0530

    moved app.js

--- controller/logic.js
+++ controller/logic.js
@@ -1,7 +1,6 @@
 const path=require('path');
 const qr=require('qr-image');
 const express=require('express');
-
 const router=express.Router();
 
 router.get('/',(req,res,next)=>{
--- functions/app.js
+++ functions/app.js
@@ -1,5 +1,23 @@
+const path=require('path');
 const serverless=require('serverless-http');
+const express=require('express');
+const bodyParser=require('body-parser');
 
-const app=require("../app");
+const app=express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));;
+
+app.use(express.static(path.join(__dirname,'..', 'public')));
+
+const routes=require('./controller/logic');
+
+
+app.use("/.netlify/functions/api",routes);
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'..','view','index.html'));
+})
+
+module.exports=app;
 module.exports.handler=serverless(app);
