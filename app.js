a45970af126b431b9f2ae910b94c1b4cf95d16a6
Sunil Ganesh Kurra <2000137243@hexaware.com>
2025-03-04 15:46:41 +0530

    after chages

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 const path=require('path');
-
 const express=require('express');
+const bodyParser=require('body-parser');
+
 const app=express();
 
-
-const bodyParser=require('body-parser');
 app.use(bodyParser.json());
-
-
 app.use(bodyParser.urlencoded({ extended: false }));;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 const routes=require('./controller/logic');
-const ServerlessHttp = require('serverless-http');
 
 
 app.use("/.netlify/functions/api",routes);
-module.exports.handler=ServerlessHttp(app);
+
+module.exports=app;
